9efd639bc4d45a4b2dd1fd62ba9cc51eac57f587
Andreyome <andreyomem@gmail.com>
2023-01-03 21:05:22 +0200

    commit 1

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -2,4 +2,31 @@ package com.epam.mjc.stage0;
 
 public class Animal {
 
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
+
+
+    public Animal( String color,int numberOfPaws,boolean hasFur){
+        this.color=color;
+        this.numberOfPaws=numberOfPaws;
+        this.hasFur=hasFur;
+    }
+    public String getDescription(){
+        String result="";
+        result+="This animal is mostly " +color+ ". It has ";
+        if (numberOfPaws!=1){
+            result+=numberOfPaws + " paws and ";
+        }
+        else{
+            result+=numberOfPaws + " paw and ";
+        }
+        if (hasFur==true){
+            result+= "a fur.";
+        }
+        else{
+            result+="no fur.";
+        }
+        return result;
+    }
 }
